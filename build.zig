9f6a899ef2a27b20e4e01815a13c286c31681001
Amir Alawi <83512437+amiralawi@users.noreply.github.com>
2024-06-18 18:00:58 -0400

    add module return fix
    _ = b.addModule ...

--- build.zig
+++ build.zig
@@ -16,7 +16,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     // Export module
-    b.addModule("zargh", .{ .root_source_file = b.path("src/zargh.zig") });
+    _ = b.addModule("zargh", .{ .root_source_file = b.path("src/zargh.zig") });
 
     const ziglyph = b.dependency("ziglyph", .{
         .optimize = optimize,
